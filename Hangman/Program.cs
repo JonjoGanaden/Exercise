c8cb74db5e29cc4de20ade3f52bfeaee2a1ed276
JonjoGanaden <jonjoganaden@hotmail.com>
2022-04-28 10:02:51 +0100

    update

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -20,6 +20,8 @@ namespace Hangman
 
             bool lettersonly = secretword.All(Char.IsLetter);
 
+
+            //make sure that the secret words only contains letters
             while (lettersonly == false || secretword.Length == 0)
             {
                 Console.WriteLine("only letters");
@@ -27,26 +29,31 @@ namespace Hangman
                 secretword = Console.ReadLine();
                 lettersonly = secretword.All(Char.IsLetter);
             }
-
+            
+            //changes all lowercase letter to capital letters
             secretword = secretword.ToUpper();
 
+            //used arrry to check how many letters are ther in the word
             int wordLength = secretword.Length;
             char[] secretArray = secretword.ToCharArray();
             char[] Arraylength = new char[wordLength];
             char[] guessedLetters = new char[32];
-            int numberStore = 0;
+            int guessStore = 0;
             bool WinCondition = false;
-            int counter = -1; 
+            int size = -1; 
 
+
+            //for every letter in the secret word change it to "-"
             foreach (char letter in Arraylength)
             {
-                counter++;
-                Arraylength[counter] = '-';
+                size++;
+                Arraylength[size] = '-';
             }
+
 
             while (lives > 0)
             {
-                counter = -1;
+                size = -1;
                 string printProgress = String.Concat(Arraylength);
                 bool letterFound = false;
                 int multiples = 0;
@@ -57,17 +64,21 @@ namespace Hangman
                     break;
                 }
 
+                //displays current lives
                 if (lives > 0)
                 {
                     Console.WriteLine("current lives {0}", lives);
                 }
 
+                //prints out the right letter guessed so far 
                 Console.WriteLine("current progress: " + printProgress);
                 Console.Write("Guess a letter: ");
                 string playerGuess = Console.ReadLine();
 
                 bool guessTest = playerGuess.All(Char.IsLetter);
 
+
+                //Will prompt if user input more than 1 letter
                 while (guessTest == false || playerGuess.Length != 1)
                 {
                     Console.WriteLine("single letter only");
@@ -76,27 +87,31 @@ namespace Hangman
                     guessTest = playerGuess.All(Char.IsLetter);
                 }
 
+                //changes users guess to uppercase
                 playerGuess = playerGuess.ToUpper();
                 char playerChar = Convert.ToChar(playerGuess);
 
+
+                //stores users guess letters
                 if (guessedLetters.Contains(playerChar) == false)
                 {
 
-                    guessedLetters[numberStore] = playerChar;
-                    numberStore++;
+                    guessedLetters[guessStore] = playerChar;
+                    guessStore++;
 
                     foreach (char letter in secretArray)
                     {
-                        counter++;
+                        size++;
                         if (letter == playerChar)
                         {
-                            Arraylength[counter] = playerChar;
+                            Arraylength[size] = playerChar;
                             letterFound = true;
                             multiples++;
                         }
 
                     }
 
+                    //condition if the player guesses a word right or wrong or reused the same letter
                     if (letterFound)
                     {
                         Console.WriteLine("Found {0} letter: {1}", multiples, playerChar);
@@ -107,16 +122,16 @@ namespace Hangman
                         lives -= 1;
                     }
                     
-                }
-                else
-                {
+                    }
+                    else
+                    {
                     Console.WriteLine("You already guessed {0}", playerChar);
-                }
+                    }
 
 
             }
 
-
+            //condition if player wins or loses
             if (WinCondition)
             {
                 Console.WriteLine("\n\nwell done");
